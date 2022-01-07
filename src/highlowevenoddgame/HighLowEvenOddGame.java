0282a96d1e8fdee4c7f2fb6fb3ccdf39ed69ff78
hquantz <hunter.quantz9302@mail.davistech.edu>
2022-01-06 19:05:31 -0700

    Created initial files

---
+++ src/highlowevenoddgame/HighLowEvenOddGame.java
@@ -0,0 +1,18 @@
+/*
+ * HighLowEvenOddGame.java
+ * Hunter Quantz
+ * 
+ * This program simulates rolling two dice and has the user guess if it will be
+ * above or below 7, with 7 always being a loss. The user also guess if the 
+ * total will be even or odd.
+ */
+package highlowevenoddgame;
+import java.util.Scanner;
+
+public class HighLowEvenOddGame {
+
+    public static void main(String[] args) {
+
+    }// end main
+
+}// end HighLowEvenOddGame class
